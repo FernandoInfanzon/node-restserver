96a45da62fdc3781cfda5491623de362252d6933
Fernando Infanzon <luisfernandoim@gmail.com>
2018-09-28 17:10:47 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,20 @@
 // Puerto
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ENTORNO
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//BD
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:Lf123456@ds123971.mlab.com:23971/cafe';
+}
+
+process.env.URLDB = urlDB;
